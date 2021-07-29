5738bf9b039b6d176b1afbfb88156be4a32a5c61
Debarun Roy <debarunroy2000@gmail.com>
2021-07-29 12:53:46 +0530

    Added searching user

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from profiles_api import permissions
+from rest_framework import filters
 
+from profiles_api import permissions
 from profiles_api import serializers
 from rest_framework import viewsets
 from profiles_api import models
@@ -109,3 +110,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
